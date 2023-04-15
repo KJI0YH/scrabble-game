b98a9924b64b7e7cfe861ea109ea103b53da813f
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-04-16 01:06:05 +0300

    change routes files

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ import './database/db.js';
 import express, { response } from 'express';
 import http from 'node:http';
 import cors from 'cors';
-import routes from './routes.js';
+
+import authRoutes from './routes/authRoutes.js';
+import defaultRoutes from './routes/defaultRoutes.js';
 
 import { config } from './config.js';
 import { handleError } from './middlewares/error.js';
@@ -13,7 +15,9 @@ const server = new http.Server(app);
 app.use(express.json());
 app.use(cors({ origin: config.ALLOW_LIST_HOSTS, credentials: true }));
 
-routes(app);
+// Routes
+app.use('/auth', authRoutes);
+app.use('/', defaultRoutes);
 
 app.use((error, _request, response, _) => {
     handleError(error, response);
