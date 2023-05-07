80751430240935c6aded33da4890279b037a82a9
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2023-05-07 03:28:42 +0300

    add styles for tile bag

--- client/src/components/TileBag/TileBag.jsx
+++ client/src/components/TileBag/TileBag.jsx
@@ -3,16 +3,27 @@ function TileBag(props) {
     const { tiles } = props;
 
     return (
-        <div className='tile-bag'>
-            {tiles.map(tile => (
-                <div className='tile-bag-cell'>
-                    <div className='tile-bag-cell-letter'>{tile.letter}</div>
-                    <div className='tile-bag-cell-count'>&times;{tile.count}</div>
+        <div className='tile-bag-container'>
+            <div className='tile-bag-header'>
+                <div>
+                    Tile bag
                 </div>
-            ))}
+                <div>
+                    {tiles.reduce((sum, tile) => {
+                        return sum + tile.count;
+                    }, 0)}
+                </div>
+            </div>
+            <div className='tiles'>
+                {tiles.map(tile => (
+                    <div className='tile-bag-cell'>
+                        <div className='tile-bag-cell-letter'>{tile.letter}</div>
+                        <div className='tile-bag-cell-count'>&times;{tile.count}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
-
 }
 
 export default TileBag;
